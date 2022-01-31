903603da30f6705a56770efcb74340518c6f1801
RafaelCV <cavr134@gmail.com>
2022-01-31 11:23:51 -0600

    update

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -15,7 +15,7 @@ const GifExpertApp = () => {
 
     return (
         <>
-            <h2>GifExpertApp</h2>
+            <h2>GifExpertApp V2</h2>
             <AddCategory setCategories = {setCategories}/>
             <hr></hr>
 
